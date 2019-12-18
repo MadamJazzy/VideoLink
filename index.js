6f6ee6f27a7441240d9776e291187c803e6761e2
MadamJazzy <22993774+MadamJazzy@users.noreply.github.com>
2019-12-18 16:45:05 -0500

    After Code change

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
-const {Plugin} = require("powercord/entities");
-const webpack = require("powercord/webpack");
-const {getModule} = webpack;
+const { Plugin } = require('powercord/entities');
+const { getModule } = require('powercord/webpack');
+
 
 module.exports = class VideoLink extends Plugin {
-    constructor() {
-        super()
-    }
-
-    async startPlugin() {
-        const getGuild = (await getModule(["getGuild"])).getGuild;
-        const getChannel = (await getModule(["getChannel"])).getChannel;
-        const VC = (await getModule(["getVoiceChannelId"])).getVoiceChannelId;
-        var channelID = '';
-        this.registerCommand(
-            "videolink",
-            ["vidlink"],
-            "Generate a Video link for any Voice Channel ID.",
-            "{c}",
-            //code
-            async(x) => {
-                const channelID = (x === null) ? channelID = `${VC()}` : channelID = `${input}`;
-                const channel = getChannel(channelID);
-                const link = (channel.guild.id) ? ('https://canary.discordapp.com/channels/' + guild.id + "/" + channelID)
-                    : "Oops, Something isn't quite right, I was not able to find that Channel ID or you don't " +
-                    "have permissions to access that channel!";
-                return {send: false, result: link}
-                    }
-        )
-    }
+  async startPlugin () {
+    const guild = await getModule([ 'getGuild' ]);
+    const getChannel = (await getModule([ 'getChannel' ]));
+    const vc = (await getModule([ 'getVoiceChannelId' ]));
+    this.registerCommand('videolink', [ 'vidlink' ], 'Generate a Video link for any Voice Channel ID.', '{c}',
+      (x) => {
+        const channelID = (x.length === 0) ? x[0] : vc();
+        const channel = getChannel(channelID);
+        const link = (channel.guild.id) ? `https://canary.discordapp.com/channels/${guild.id}/${channelID}`
+          : 'Uh oh, something went wrong.';
+        return {
+          send: false,
+          result: link
+        }
+      })
+  }
 };

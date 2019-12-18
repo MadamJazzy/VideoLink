99e569acd3124cc49d0feed7f7a75a5e6916e3e3
MadamJazzy <22993774+MadamJazzy@users.noreply.github.com>
2019-12-18 17:17:25 -0500

    Temp Roll Back

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const { Plugin } = require('powercord/entities');
-const { getModule } = require('powercord/webpack');
-
+const webpack = require('powercord/webpack');
+const { getModule } = webpack;
 
 module.exports = class VideoLink extends Plugin {
   async startPlugin () {
-    const guild = await getModule([ 'getGuild' ]);
-    const getChannel = (await getModule([ 'getChannel' ]));
-    const vc = (await getModule([ 'getVoiceChannelId' ]));
+    const { getGuild } = await getModule([ 'getGuild' ]);
+    const { getChannel } = await getModule([ 'getChannel' ]);
     this.registerCommand('videolink', [ 'vidlink' ], 'Generate a Video link for any Voice Channel ID.', '{c}',
-      (x) => {
-        const channelID = (x.length === 0) ? x[0] : vc();
+      async (x) => {
+        const channelID = `${x}`;
         const channel = getChannel(channelID);
-        const link = (channel.guild.id) ? `https://canary.discordapp.com/channels/${guild.id}/${channelID}`
-          : 'Uh oh, something went wrong.';
+        const guild = getGuild(channel.guild_id);
+        if (guild.id) {
+          return {
+            send: false,
+            result: (`https://canary.discordapp.com/channels/${guild.id}/${channelID}`)
+          };
+        }
         return {
           send: false,
-          result: link
-        }
-      })
+          result: 'Link Generation Failed!'
+        };
+      }
+    );
   }
 };

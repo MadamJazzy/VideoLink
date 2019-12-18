43ebd6dab17110cf5794eb91ee5b90236aa1a3af
MadamJazzy <22993774+MadamJazzy@users.noreply.github.com>
2019-12-18 04:05:03 -0500

    Change in coding

--- videolink.js
+++ videolink.js
@@ -1,13 +1,10 @@
-const { Plugin } = require("powercord/entities")
+const { Plugin } = require("powercord/entities");
 
-module.exports = class Vidlink extends Plugin {
-  startPlugin() {this.registerCommand("vidlink", [], "Video Link Generator", "{c} [channelID]", (args) => ({send: false, result: this.vidlink(args)})); }
-  
-  vidlink(args) {
-	  
-	  let guildID = channel.guild_id;
-	  let channelID = input
-	  let msg = "https://canary.discordapp.com/channels${guildID !== null ? `/${guildID}` : /${channelID}"
-	  return msg
-	  }
-	  
+module.exports = class videolink extends Plugin {
+  startPlugin() {this.registerCommand("videolink", [], "Video Link Generator", "{c} [channelID]", (args) => ({send: false, result: this.vidlink(args)})); }
+  videolink(args) {
+  	let guildID = channel.guild_id;
+	let channelID = input;
+	let msg = "https://canary.discordapp.com/channels${guildID !== null ? `/${guildID}` : /${channelID}"
+	return msg
+  }

309dc334a673540381ff67d420bb32c7fe97555b
MadamJazzy <22993774+MadamJazzy@users.noreply.github.com>
2019-12-18 23:01:16 -0500

    Final Push
    Working Totally. With both if already in VC and if given a ChannelID

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const { Plugin } = require('powercord/entities');
 const { getModule } = require('powercord/webpack');
-const voice = getModule([ 'getVoiceChannelId' ]).getVoiceChannelId;
 
 module.exports = class VideoLink extends Plugin {
   async startPlugin () {
-    this.registerCommand('videolink', [], 'Video Chat Link Generator', '{c} [channel_ID]',
-      async (input) => {
-        const { getGuild } = await getModule([ 'getGuild' ]),
-          { getChannel } = await getModule([ 'getChannel' ]),
-          guild = getGuild((getChannel(`${input}`)).guild_id),
-          link = (`https://canary.discordapp.com/channels/${guild.id}/${input}`);
+    const
+      { getChannel } = getModule([ 'getChannel' ], false),
+      { getVoiceChannelId } = getModule([ 'getVoiceChannelId' ], false);
+    this.registerCommand('videolink', [ 'vidlink' ], 'Video Chat Link Generator', '{c} [channel_ID]',
+      args => {
+        const channelID = args.length > 0 ? args[0] : getVoiceChannelId();
+        if (!channelID) {
+          return {
+            send: false,
+            result: 'Do I look clairvoyant to you? It seems you are not in a VC and you did not tell me a channel ID'
+          };
+        }
+        const channel = getChannel(channelID);
+        // eslint-disable-next-line multiline-ternary
+        const link = channel ? `https://discordapp.com/channels/${channel.guild_id}/${channelID}`
+          : `Oops, I wasn't able to find a channel with the id \`${channelID}\`.\nMake sure you provide a valid channel id!`;
         return {
           send: false,
-          result: `${link}`
+          result: link
         };
       });
-    /*
-     * this.registerCommand('voicelink', [], 'Video link for the Channel you are currently in (voice)', '{C}'
-     * async (ton) => {
-     * const { getVoice } = await getModule([ 'getVoiceChannelId' ]).getVoiceChannelId
-     * const
-     */
   }
 };
--- readme.md
+++ readme.md
@@ -4,4 +4,4 @@ This is a permission viewer plugin made for [Powercord](https://github.com/power
 ## Installation
 **git clone https://github.com/MadamJazzy/VideoLink VideoLink**
 
-
+*Brought to you by Sapphire#0419 and NurMarvin(assistance with checking if members is already in a VC. Thx man)*
